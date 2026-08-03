f36b726d62beeb3c695d52281bec806b10cb02b7
Kole Johnson <flamingcoder05@gmail.com>
2026-08-03 18:43:17 -0500

    New React Website Project added react router for navigation and test pages

---
+++ src/screens/about.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function About() {
+    return (
+        <div>
+            <h1>Test page for navigation</h1>
+            <Link to='/' >Home</Link>
+        </div>
+    )
+
+}
---
+++ src/screens/defaultScreen.js
@@ -0,0 +1,12 @@
+import logo from '../logo.svg';
+import '../App.css';
+import { Link } from 'react-router-dom';
+
+export default function Default() {
+  return (
+    <div className="Default-View">
+        <h1>Welcome to the beginning of Clarence the Tractor</h1>
+        <Link to="about">About</Link>
+    </div>
+  );
+}
